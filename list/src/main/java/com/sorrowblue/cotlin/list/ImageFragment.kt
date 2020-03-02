18990eca2a5a44cc5973a8fcf0a1ac071a5ce8f2
SorrowBlue <sorrowblue.dev@gmail.com>
2020-03-03 01:13:41 +0900

    :tada:
    Signed-off-by: SorrowBlue <sorrowblue.dev@gmail.com>

--- list/src/main/java/com/sorrowblue/cotlin/list/ImageFragment.kt
+++ list/src/main/java/com/sorrowblue/cotlin/list/ImageFragment.kt
@@ -10,37 +10,37 @@ import com.sorrowblue.cotlin.ui.fragment.DataBindingFragment
 import com.sorrowblue.cotlin.ui.fragment.toolbar
 
 internal class ImageFragment :
-    DataBindingFragment<ImageFragmentMainBinding>(R.layout.image_fragment_main) {
+	DataBindingFragment<ImageFragmentMainBinding>(R.layout.image_fragment_main) {
 
-    private lateinit var adapter: ImagePagerAdapter
-    private val args: ImageFragmentArgs by navArgs()
+	private lateinit var adapter: ImagePagerAdapter
+	private val args: ImageFragmentArgs by navArgs()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        sharedElementEnterTransition = MaterialContainerTransform(requireContext())
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		sharedElementEnterTransition = MaterialContainerTransform(requireContext())
+	}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        postponeEnterTransition()
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		postponeEnterTransition()
 //        ViewCompat.setTransitionName(binding.viewPager2, "imageView_${args.position}")
-        adapter = ImagePagerAdapter().apply {
-            setList(args.images.asList())
-        }
-        binding.viewPager2.adapter = adapter
-        Log.d(javaClass.simpleName, "position ${args.position}")
-        binding.viewPager2.setCurrentItem(args.position, false)
-        binding.viewPager2.viewTreeObserver.addOnPreDrawListener {
-            startPostponedEnterTransition()
-            true
-        }
-        binding.edit.setOnClickListener {
-            adapter.setRotation(binding.viewPager2.currentItem, -90)
-        }
-    }
+		adapter = ImagePagerAdapter().apply {
+			setList(args.images.asList())
+		}
+		binding.viewPager2.adapter = adapter
+		Log.d(javaClass.simpleName, "position ${args.position}")
+		binding.viewPager2.setCurrentItem(args.position, false)
+		binding.viewPager2.viewTreeObserver.addOnPreDrawListener {
+			startPostponedEnterTransition()
+			true
+		}
+		binding.edit.setOnClickListener {
+			adapter.setRotation(binding.viewPager2.currentItem, -90)
+		}
+	}
 
-    override fun onStart() {
-        super.onStart()
-        toolbar.title = args.images[args.position].name
-    }
+	override fun onStart() {
+		super.onStart()
+		toolbar.title = args.images[args.position].name
+	}
 }
